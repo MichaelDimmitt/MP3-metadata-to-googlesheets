8db1856aed7865da61f3d7c5c27e63f5c0a57aa6
michael <michaelgdimmitt@gmail.com>
2024-01-13 15:43:31 -0500

    add new books to the array

--- README.md
+++ README.md
@@ -11,6 +11,9 @@ ffmpeg - and ffprobe which comes as part of ffmpeg
 https://askubuntu.com/a/754922/684192
 https://stackoverflow.com/questions/34745451/passing-arguments-to-jq-filter
 https://stackoverflow.com/a/47024050/5283424
+https://unix.stackexchange.com/questions/460985/jq-add-objects-from-file-into-json-array
 
 I also used `tldr jq` to get a high level understanding of jq  
 https://github.com/tldr-pages/tldr
+
+https://how.wtf/add-new-element-to-json-array-with-jq.html
--- get-mp3-data.bash
+++ get-mp3-data.bash
@@ -8,7 +8,19 @@ get-mp3-data() {
   local book="The Final Empire: Mistborn Book 1.mp3"
   local jqObject=$(ffprobe -v quiet -print_format json -show_format -show_streams /Volumes/"Samsung USB"/Audiobooks/"$book" 2>&1);
   local duration=$(echo "$jqObject" | jq '.format.duration');
-  echo "$jqObject" | jq --arg v "$duration" '.format.tags + {'duration': $v}' | jq 'del(.copyright)';
+  local bookJSON=$(echo "$jqObject" | jq --arg v "$duration" '.format.tags + {'duration': $v}' | jq 'del(.copyright)') 
+
+  # precondition the json file being operated on should have a beginning value []
+  # add book to json
+  # temp file needs to be used because we cant write to our input file right away ... it gets currupted and becomes blank!
+  cat books.json | jq --argjson v "$bookJSON" '. += [$v]' > temp.json
+  cp temp.json books.json
+  
+  # convert json to csv
+  jq -r '(map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv' books.json > temp.csv
+  
+  cat books.json | jq length
+
 };
 get-mp3-data
 
